71bc4283d625380bd4dedc305c59e938807a8cc3
Matthew Worthington <worthingtown@gmail.com>
2017-12-21 21:59:55 +0000

    Update to use dapper over entity

--- SiliconPanda.Shared.Core/Data/ICrud.cs
+++ SiliconPanda.Shared.Core/Data/ICrud.cs
@@ -6,18 +6,11 @@ namespace SiliconPanda.Shared.Core.Data
 {
     public interface ICrud
     {
-        Task<TEntity> FindAsync<TEntity, TIdentifier>(TIdentifier id);
-
-        Task<IQueryable<TEntity>> QueryAsync<TEntity>();
-
-        Task<TEntity> InsertAsync<TEntity, TIdentifier>(TEntity entity);
-
-        Task<int> UpdateAsync<TEntity>(TEntity entity);
-
-        Task<bool> DeleteAsync<TEntity>(TEntity entity);
-
-        Task<bool> DeleteAsync<TEntity, TIdentity>(TIdentity id);
-
-        Task<IList<TEntity>> GetPagedAsync<TEntity>(IQueryable<TEntity> query, int page, int take);
+        Task<TEntity> FindAsync<TEntity>(Guid id) where TEntity : BaseEntity;
+        Task<IEnumerable<TEntity>> QueryAsync<TEntity>(string sql, Type[] types, Func<object[], TEntity> map, object param = null) where TEntity : BaseEntity;
+        Task<TEntity> InsertAsync<TEntity>(TEntity entity) where TEntity : BaseEntity;
+        Task<int> UpdateAsync<TEntity>(TEntity entity) where TEntity : BaseEntity;
+        Task<bool> DeleteAsync<TEntity>(Guid id) where TEntity : BaseEntity;
+        Task<int> DeleteListAsync<TEntity>(object whereConditions) where TEntity : BaseEntity;
     }
 }
