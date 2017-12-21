be4405d4695443584bed6ea3964ab179757713ce
Matthew Worthington <worthingtown@gmail.com>
2017-12-21 22:06:13 +0000

    adding references

--- SiliconPanda.Shared.Core/Data/ICrud.cs
+++ SiliconPanda.Shared.Core/Data/ICrud.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using SiliconPanda.Shared.Domain.Dto;
 
 namespace SiliconPanda.Shared.Core.Data
 {
